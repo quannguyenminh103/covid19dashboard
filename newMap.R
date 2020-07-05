43fcdd1d449b22173c085cab7b6276863d7fbae6
Quan Minh Nguyen <quannguyenminh103@gmail.com>
2020-07-05 18:31:07 -0400

    create animation maps

--- newMap.R
+++ newMap.R
@@ -30,7 +30,7 @@ MergedGA_today$subregion = capitalize(MergedGA_today$subregion)
 dataTracking = read.csv('./dataInput/COVID-19-Activity.csv', as.is = TRUE)
 dataTracking$COUNTY_NAME <- tolower(dataTracking$COUNTY_NAME)
 dataTracking[which(dataTracking == 'dekalb', arr.ind = TRUE)] <- 'de kalb'
-head(dataTracking)
+#head(dataTracking)
 
 georgiaIndex <- which(dataTracking[,4] == 'Georgia', arr.ind = TRUE)
 georgiadataTracking <- dataTracking[georgiaIndex,]
@@ -57,71 +57,54 @@ create_png <- function(rdate){
   subdata <- statistics(rdate)
   # combined two tables: This may no include the Non-Georgia Resident and Unknown.
   MergedGA <- inner_join(GAmap, subdata, by = c("subregion" = "COUNTY_NAME"))
-  county <- map_data("county")
-  gacounty <- county[which(county[,5] == 'georgia'),]
-  gg <- ggplot()
-  gg <- gg + geom_map(data=gacounty, map=gacounty,
-                      aes(long, lat, map_id=region),
-                      color="#2b2b2b", fill=NA, size=0.15)
-  gg <- gg + coord_map("polyconic")
-  gg <- gg + theme_map()
-  gg <- gg + theme(plot.margin=margin(20,20,20,20))
-  
-  positive_map <- gg
-  positive_map <- positive_map + geom_polygon( data=MergedGA, 
+  positive_map <- ggplot() + geom_polygon( data=MergedGA, 
                                                aes(long, lat, group=group, fill = Positive), 
-                                               color="white", size = 0.2)
+                                               color="black", size = 0.2)
   positive_map <- positive_map + scale_fill_continuous(name="Level of Positive Cases", 
                                                        low = "lemonchiffon", high = "firebrick",limits = c(0,max(MergedGA_today$Positive)), 
-                                                       breaks=c(2,10000,2000,3000,4000,5000,6000), na.value = "grey50") +
-    labs(title=paste('Georgia Confirmed Cases (', rdate, ')', sep='')) + theme(legend.position = "right")
-  ggsave(paste('./www/Map_', rdate, '.png', sep=''),
+                                                       breaks=quantile(MergedGA_today$Positive,c(0,0.2,0.3,0.5,0.6,0.7,0.8,0.85,0.88,0.9,0.93,0.98,0.99,1)), na.value = "grey50") +
+    coord_map() + theme_map() +  
+    labs(title=paste('Georgia Confirmed Cases (', rdate, ')', sep='')) +
+    theme(plot.title = element_text(face = "bold")) +
+    theme(legend.position = 'none') 
+  ggsave(paste('./www/PositiveMap/PC_GA_Map_', rdate, '.png', sep=''),
          width = 6, height = 3.1, units = 'in', dpi=150, pointsize=14)
   return(positive_map)
 }
 # Read the individual maps into a data structure for use with 'magick'
 
-#date_range <- seq(as.Date("2020-03-01"), Sys.Date()-1, "days")
-# for (val in 1:length(date_range)){
-#   create_png(toString(date_range[val]))
-# }
-
-# GEORGIA CONFIRMED CASES MAP TODAY:
-county <- map_data("county")
-gacounty <- county[which(county[,5] == 'georgia'),]
-gg_today <- ggplot()
-gg_today <- gg_today + geom_map(data=gacounty, map=gacounty,
-                                aes(long, lat, map_id=region),
-                                color="#2b2b2b", fill=NA, size=0.15)
-gg_today <- gg_today + coord_map("polyconic")
-gg_today <- gg_today + theme_map()
-gg_today <- gg_today + theme(plot.margin=margin(20,20,20,20))
-positive_map_today <- gg_today
-positive_map_today <- positive_map_today + geom_polygon( data=MergedGA_today, 
+date_range <- seq(as.Date("2020-03-01"), Sys.Date()-1, "days")
+for (val in 1:length(date_range)){
+  create_png(toString(date_range[val]))
+}
+# GEORGIA CONFIRMED CASES MAP TODAY: c(2,100,300,500,700,800,1000,3000,5000,6000,7000,9000)
+positive_map_today <- ggplot() + geom_polygon( data=MergedGA_today, 
                                                          aes(x=long, y=lat, group=group, fill = Positive), 
-                                                         color="white", size = 0.2)
+                                                         color="black", size = 0.2)
 positive_map_today <- positive_map_today + scale_fill_continuous(name= "Level of Positive Cases", 
                                                                  low = "lemonchiffon", high = "firebrick",limits = c(0,max(MergedGA_today$Positive)), 
-                                                                 breaks=c(2,10000,2000,3000,4000,5000,6000), na.value = "grey50") +
-  labs(title=paste('Georgia Confirmed Cases (', Sys.Date()-1, ')', sep='')) + theme(legend.position = "right")
-ggsave(paste('./www/Map_', Sys.Date(), '.png', sep=''),
+                                                                 breaks= quantile(MergedGA_today$Positive,c(0,0.2,0.3,0.5,0.6,0.7,0.8,0.85,0.88,0.9,0.93,0.98,0.99,1)), na.value = "grey50") +
+  coord_map() + theme_map() +  
+  labs(title=paste('Georgia Confirmed Cases (', Sys.Date(), ')', sep='')) +
+  theme(plot.title = element_text(face = "bold")) +
+  theme(legend.position = 'none')
+ggsave(paste('./www/PositiveMap/PC_GA_Map_', Sys.Date(), '.png', sep=''),
        width = 6, height = 3.1, units = 'in', dpi=150, pointsize=14)
-
 ### REMEMBER CHANGE SYS.DATE() - 1 to SYS.DATE()
 
-imglayers <- function(rdate){
-  image_read(paste('./www/Map_', x,'.png', sep=''))
-}
+# imglayers <- function(rdate){
+#   image_read(paste('./www/PositiveMap/PC_GA_Map_', x,'.png', sep=''))
+# }
 
 date_range2 <- seq(as.Date("2020-03-01"), Sys.Date(), "days")
 
 imglayers <- sapply(date_range2, function(x) {
-  image_read(paste('./www/Map_', x,'.png', sep=''))
+  image_read(paste('./www/PositiveMap/PC_GA_Map_', x,'.png', sep=''))
 })
   
   # Generate an animated GIF with the individual maps and write to a file
 imganim <- image_animate(image_join(imglayers), fps = 10, dispose = "previous")
-mapanim <- image_write(imganim, './GAConfirmedCasesMap.gif')  
+mapanim <- image_write(imganim, './www/GAConfirmedCasesMap.gif')  
   
   
   
