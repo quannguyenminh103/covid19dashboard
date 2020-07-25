bad7afe329389d3f93f91319c169aa08296a670a
Quan Minh Nguyen <quannguyenminh103@gmail.com>
2020-07-24 20:45:17 -0400

    data Input

--- dataInput/Auto Download and Unzip Data Files from Websites.py
+++ dataInput/Auto Download and Unzip Data Files from Websites.py
@@ -6,17 +6,17 @@ def main():
         z.extractall()
     download_file('https://ga-covid19.ondemand.sas.com/docs/ga_covid_data.zip')
 
-    csv_url = 'https://covid19-lake.s3.us-east-2.amazonaws.com/tableau-covid-datahub/csv/COVID-19-Activity.csv'
-    req = requests.get(csv_url)
-    url_content = req.content
-    csv_file = open('COVID-19-Activity.csv', 'wb')
-    csv_file.write(url_content)
-    csv_file.close()
-
-    csv_url = 'https://covidtracking.com/api/v1/states/ga/daily.csv'
-    req = requests.get(csv_url)
-    url_content = req.content
-    csv_file = open('daily.csv', 'wb')
-    csv_file.write(url_content)
-    csv_file.close()
+##    csv_url = 'https://covid19-lake.s3.us-east-2.amazonaws.com/tableau-covid-datahub/csv/COVID-19-Activity.csv'
+##    req = requests.get(csv_url)
+##    url_content = req.content
+##    csv_file = open('COVID-19-Activity.csv', 'wb')
+##    csv_file.write(url_content)
+##    csv_file.close()
+##
+##    csv_url = 'https://covidtracking.com/api/v1/states/ga/daily.csv'
+##    req = requests.get(csv_url)
+##    url_content = req.content
+##    csv_file = open('daily.csv', 'wb')
+##    csv_file.write(url_content)
+##    csv_file.close()
 main()
